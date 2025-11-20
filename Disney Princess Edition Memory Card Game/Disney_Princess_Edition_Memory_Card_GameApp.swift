793f26f103c70d0890bc386ae65fb8f8285d4dc1
adinsmore <adinsmore@bsd220.org>
2025-11-20 11:57:33 -0600

    Initial Commit

---
+++ Disney Princess Edition Memory Card Game/Disney_Princess_Edition_Memory_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disney_Princess_Edition_Memory_Card_GameApp.swift
+//  Disney Princess Edition Memory Card Game
+//
+//  Created by Alissa Dinsmore on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Disney_Princess_Edition_Memory_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
